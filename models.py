0d22c5ee25c6d41c3bb913e6a19a5ac3570cbd8c
KenCorp <kennethwarui35@gmail.com>
2023-09-04 11:49:12 +0300

    Created tables

--- models.py
+++ models.py
@@ -1,17 +1,40 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Integer, String, Column, ForeignKey
+from sqlalchemy import Integer, String, Column, ForeignKey, Table
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import sessionmaker
+
 
 engine = create_engine('sqlite:///review.db')
-
+Session = sessionmaker(bind=engine)
+session = Session()
 Base = declarative_base()
+#  many to many
+# creating table
+customer_restaurant = Table('customer_restaurant', Base.metadata, Column('restaurant_id', ForeignKey('restaurant.id'), primary_key = True), Column('customer_id', ForeignKey('customer.id'), primary_key = True))
 
-class Restaurnats(Base):
+class Restaurants(Base):
     __tablename__ = 'restaurant'
 
     id = Column(Integer(), primary_key=True)
     name = Column(String())
     price = Column(Integer())
+
+    #  one to many
+    reviews = relationship('Review', backref=backref('restaurant'))
+    # many to many with customers
+    customers = relationship('Customer', secondary=customer_restaurant, back_populates='restaurant')
+
+class Review(Base):
+    __tablename__ = 'review'
+
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Integer)
+    restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
+    customer_id = Column(Integer, ForeignKey('customer.id'))
+
+    # many to one relation
+    customer = relationship('Customer', backref='reviews')
 
 
 class Customer(Base):
@@ -20,3 +43,11 @@ class Customer(Base):
     id = Column(Integer(), primary_key=True)
     first_name = (String())
     last_name = (String())
+
+
+    restaurants = relationship('Restaurant', secondary=customer_restaurant, backref='customers')
+
+
+    
+    
+

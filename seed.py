2ee623eb5f8533c5cf790ad022c27143e7f17542
KenCorp <kennethwarui35@gmail.com>
2023-09-05 09:36:35 +0300

    fixed backref name

--- seed.py
+++ seed.py
@@ -17,28 +17,28 @@ if __name__ == '__main__':
         new_customer = Customer(first_name=fake.name(),
         last_name=fake.last_name()
         )
-    #     session.add(new_customer)
-    #     print('customers counted')
+        session.add(new_customer)
+        print('customers counted')
 
-    # print('Now counting restaurants')
-    # for restaurant in range(7):
-    #     restaurant = Restaurant(
-    #         name=fake.company(),
-    #         price=fake.random_int(min=1, max=7)
+    print('Now counting restaurants')
+    for restaurant in range(7):
+        restaurant = Restaurant(
+            name=fake.company(),
+            price=fake.random_int(min=1, max=7)
 
-    #     )
-    #     session.add(restaurant)
+        )
+        session.add(restaurant)
 
-    # for x in range(20):
-    #     review = Review(
-    #         star_rating=fake.random_int(min=1, max=5),
-    #         customer_id=fake.random_int(min=1, max=10),  
-    #         restaurant_id=fake.random_int(min=1, max=5)
-    #     )
-    #     session.add(review)
+    for x in range(20):
+        review = Review(
+            star_rating=fake.random_int(min=1, max=5),
+            customer_id=fake.random_int(min=1, max=10),  
+            restaurant_id=fake.random_int(min=1, max=5)
+        )
+        session.add(review)
 
-    # session.commit()
-    # print ('Reviews counted')
+    session.commit()
+    print ('Reviews counted')
 
 
 
